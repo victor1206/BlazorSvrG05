12f5338d27c568df2b55bce01c2012ffbfd4e7a2
victor1206 <victor_rivas89@hotmail.com>
2024-09-24 14:11:11 -0600

    Primer Cambio

---
+++ BlazorApp2G05/Data/Estado.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp2G05.Data
+{
+    public class Estado
+    {
+        public byte Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
---
+++ BlazorApp2G05/Data/RolDTO.cs
@@ -0,0 +1,17 @@
+﻿using SysSeguridadG05.EN;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp2G05.Data
+{
+    public class RolDTO
+    {
+        public int Id { get; set; }
+
+        public String Nombre { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        List<Usuario> usuarios { get; set; }
+    }
+}
